bbc54f8e15fe25e3b4bef79c6b42b2d00b4be361
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-04 17:48:07 +0100

    agregar page Menú basica

---
+++ marea-restaurante/src/app/menu/page.jsx
@@ -0,0 +1,38 @@
+// import Image from "next/image";
+import menu from "./menu-data.json";
+
+const precio = menu.find((item) => item.precio)?.precio;
+const Menu = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="flex bg-primario justify-center p-8">
+        <h1 className="text-white text-8xl">MAREA</h1>
+        {/* <img src="" alt="Imagen del mar" /> */}
+      </header>
+      <main className="flex flex-col items-center  bg-fondo flex-1 p-6 gap-6">
+        {menu
+          .filter((item) => item.platos)
+          .map((categoria) => (
+            <div key={categoria.categoria}>
+              <div>
+                {(categoria.categoria === "Principales" ||
+                  categoria.categoria === "Postres") && <div>1</div>}
+              </div>
+              {categoria.platos.map((plato) => (
+                <div key={plato.id}>
+                  <h3>{plato.nombre}</h3>
+                  <p>{plato.descripcion}</p>
+                </div>
+              ))}
+            </div>
+          ))}
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-xl text-primario">{precio}</p>
+          <p> (IVA incluido)</p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Menu;
