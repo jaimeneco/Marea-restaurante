834b1ad7a2079193202a51380f0be8d3f52419c0
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-12 09:56:38 +0100

    añadir imgs y colocar img del mar en Menu

--- marea-restaurante/src/app/menu/page.jsx
+++ marea-restaurante/src/app/menu/page.jsx
@@ -1,13 +1,20 @@
-// import Image from "next/image";
+import Image from "next/image";
 import menu from "./menu-data.json";
 
 const precio = menu.find((item) => item.precio)?.precio;
 const Menu = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="flex bg-primario justify-center p-8">
-        <h1 className="text-white text-8xl">MAREA</h1>
+      <header className="flex flex-col bg-primario justify-center ">
+        <h1 className="text-white text-8xl mx-auto p-8">MAREA</h1>
         {/* <img src="" alt="Imagen del mar" /> */}
+        <Image
+          src={"/imgs/B&W-sea.webp"}
+          alt="Imagen del mar"
+          width={2000}
+          height={200}
+          className="object-cover max-h-40"
+        />
       </header>
       <main className="flex flex-col items-center  bg-fondo flex-1 p-6 gap-6">
         {menu
