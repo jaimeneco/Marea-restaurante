0c192d9829800a19d8a4e522ea19c631cbb4669b
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-03-12 11:37:59 +0100

    realizar layout page Menu responsive

--- marea-restaurante/src/app/menu/page.jsx
+++ marea-restaurante/src/app/menu/page.jsx
@@ -1,34 +1,39 @@
 import Image from "next/image";
 import menu from "./menu-data.json";
 
-const precio = menu.find((item) => item.precio)?.precio;
 const Menu = () => {
+  const precio = menu.find((item) => item.precio)?.precio;
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen w-full">
       <header className="flex flex-col bg-primario justify-center ">
-        <h1 className="text-white text-8xl mx-auto p-8">MAREA</h1>
+        <h1 className="text-white text-8xl lg:text-[10rem] mx-auto p-4">
+          MAREA
+        </h1>
         {/* <img src="" alt="Imagen del mar" /> */}
         <Image
           src={"/imgs/B&W-sea.webp"}
           alt="Imagen del mar"
           width={2000}
           height={200}
-          className="object-cover max-h-40"
+          className="object-cover max-h-24 lg:max-h-32 w-full"
         />
       </header>
-      <main className="flex flex-col items-center  bg-fondo flex-1 p-6 gap-6">
+      <main className="flex flex-col items-center  bg-fondo flex-1 p-6">
         {menu
           .filter((item) => item.platos)
           .map((categoria) => (
             <div key={categoria.categoria}>
-              <div>
+              <div className="my-2">
                 {(categoria.categoria === "Principales" ||
-                  categoria.categoria === "Postres") && <div>1</div>}
+                  categoria.categoria === "Postres") && (
+                  <span className="h-1 w-40 bg-secundario block mx-auto"></span>
+                )}
               </div>
               {categoria.platos.map((plato) => (
-                <div key={plato.id}>
-                  <h3>{plato.nombre}</h3>
-                  <p>{plato.descripcion}</p>
+                <div key={plato.id} className="py-4 flex flex-col gap-1 ">
+                  <h3 className="text-lg text-center">{plato.nombre}</h3>
+                  <p className="text-sm text-center">{plato.descripcion}</p>
                 </div>
               ))}
             </div>
